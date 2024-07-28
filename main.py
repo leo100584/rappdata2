2274bd77fa7fcd8995b864576a4040f6c7bc4510
leo100584 <54706498+leo100584@users.noreply.github.com>
2024-07-28 13:56:13 +0200

    Update main.py
    Added azure blob storage template

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime
 import time
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 def showTime():
     print("Hello wolrd from Azure functions!")
@@ -7,6 +8,16 @@ def showTime():
     now = datetime.datetime.now()
     # Print the current date and time in a specific format
     print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    connection_string = "din_azure_storage_connection_string"
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client("din_container", "din_blob")
+
+    data = "Ditt innhold her"  # Erstatt dette med innholdet i Excel-dokumentet ditt
+
+    blob_client.upload_blob(data)
+    showTime()
 
 if __name__ == '__main__':
   showTime()
